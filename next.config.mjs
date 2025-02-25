2142eb7c80059c47dc015b48bb24ee3633d899be
Henry <mbamaluhenry8@gmail.com>
2025-02-25 05:52:33 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(mp3|wav)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next/static/sounds/",
-            outputPath: "static/sounds/",
-            name: "[name].[hash].[ext]",
-          },
-        },
-      });
-  
-      return config;
-    },
-  };
-  
+const nextConfig = {};
+
 export default nextConfig;
